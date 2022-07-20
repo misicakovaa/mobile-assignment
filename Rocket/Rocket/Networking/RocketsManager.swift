dfe0e565004d3d097d7a43c56f822f8feb60dfa4
AdelaMisicakova <misicakovaa@quanti.cz>
2022-07-20 16:06:41 +0200

    Combine Rockets manager

--- Rocket/Rocket/Networking/RocketsManager.swift
+++ Rocket/Rocket/Networking/RocketsManager.swift
@@ -6,19 +6,48 @@
 //
 
 import Foundation
+import Combine
 
 class RocketsManager: ObservableObject {
     
     @Published var rockets = [Rocket]()
     @Published var isLoading = true
     
-    private var rocketsFetcher = RocketsFetcher()
+    var cancellables = Set<AnyCancellable>()
     
     //MARK: - Fetch rockets from spacexdata
     
     func fetchRockets() async {
-        let urlString = "https://api.spacexdata.com/v3/rockets"
-        self.rockets = await rocketsFetcher.performRequest(urlString: urlString)
+        guard let url = URL(string: "https://api.spacexdata.com/v3/rockets") else {return}
+        await getRockets(url: url)
         isLoading = false
     }
+    
+    //MARK: - Fetch rockets data using combine
+    
+    private func getRockets(url: URL) async {
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .tryMap(handleOutput)
+            .decode(type: [Rocket].self, decoder: JSONDecoder())
+            .replaceError(with: [])
+            .sink(receiveValue: { [weak self] (returnedRockets) in
+                self?.rockets = returnedRockets
+            })
+            .store(in: &cancellables)
+    }
+    
+    //MARK: - Handle DataTaskPublisher output
+    
+    private func handleOutput(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard
+            let response = output.response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode < 300 else {
+            throw URLError(.badServerResponse)
+        }
+        return output.data
+    }
 }
+
